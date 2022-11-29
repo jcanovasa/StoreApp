c6b47c4e67b451ea30437c93aa314c2818a6b1c9
jcanovasa <jaimecanovas.a@gmail.com>
2022-11-29 13:58:50 +0100

    delete method fixed, now delete users and reload the webpage

--- src/main/java/com/javaschool/project/controllers/UserController.java
+++ src/main/java/com/javaschool/project/controllers/UserController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 //import com.javaschool.project.exceptions.ResourceNotFoundException;
 import com.javaschool.project.exceptions.ResourceNotFoundException;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +14,7 @@ import com.javaschool.project.models.User;
 import com.javaschool.project.repository.UserRepository;
 
 @RestController
-@RequestMapping("/api/users/")
+@RequestMapping("/api/users")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	
@@ -20,26 +22,26 @@ public class UserController {
 	private UserRepository repository;
 
 	//Con este método listo a todos los usuarios
-	@GetMapping("/users")
+	@GetMapping("")
 	public List<User> listAllUsers() {
 		return repository.findAll();
 	}
 
 	//Con este método guardo un nuevo usuario y mando el objeto user como json
-	@PostMapping("/users")
+	@PostMapping("")
 	public User saveUser(@RequestBody User user) {
 		return repository.save(user);
 	}
 
 	//Con este selecciono un usuario en concreto
-	@GetMapping("/users/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable Long id) {
 		User user = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found)"));
 		return ResponseEntity.ok(user);
 	}
 
-	@PutMapping("/users/users/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
 		User user = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException(("User with id " + id + " does not found")));
@@ -50,5 +52,15 @@ public class UserController {
 
 		User updatedUser = repository.save(user);
 		return ResponseEntity.ok(user);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<JSONObject> deleteUser(@PathVariable Long id) {
+		System.out.println("hola3-------------------------------------------------------------------");
+		repository.deleteById(id);
+		JSONObject respuesta = new JSONObject();
+		respuesta.put("msg", ":)");
+		System.out.println(respuesta.get("msg"));
+		return new ResponseEntity<JSONObject>(respuesta, HttpStatus.OK);
 	}
 }
